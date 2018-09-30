ce8dd6849ad02da68c6d204dec70de7a8f9ef442
gt6707a <gt6707a@gmail.com>
2018-09-30 12:32:15 -0700

    add settings to set user name

--- app/src/main/java/com/android/gt6707a/orderup/OrderActivity.java
+++ app/src/main/java/com/android/gt6707a/orderup/OrderActivity.java
@@ -1,7 +1,11 @@
 package com.android.gt6707a.orderup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.order_up_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.actions_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/android/gt6707a/orderup/SettingsFragment.java
@@ -0,0 +1,108 @@
+package com.android.gt6707a.orderup;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
+
+/** A simple {@link Fragment} subclass. */
+public class SettingsFragment extends PreferenceFragmentCompat
+    implements SharedPreferences.OnSharedPreferenceChangeListener,
+        Preference.OnPreferenceChangeListener {
+
+  public SettingsFragment() {
+    // Required empty public constructor
+  }
+
+  @Override
+  public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    addPreferencesFromResource(R.xml.settings);
+
+    SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+    PreferenceScreen prefScreen = getPreferenceScreen();
+
+    int count = prefScreen.getPreferenceCount();
+
+    // Go through all of the preferences, and set up their preference summary.
+    for (int i = 0; i < count; i++) {
+      Preference p = prefScreen.getPreference(i);
+
+      if (!(p instanceof CheckBoxPreference)) {
+        String value = sharedPreferences.getString(p.getKey(), "");
+        setPreferenceSummary(p, String.format("All your orders belong to %s", value));
+      }
+    }
+
+    Preference preference = findPreference(getString(R.string.settings_user_name_key));
+    preference.setOnPreferenceChangeListener(this);
+  }
+
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+    // Figure out which preference was changed
+    Preference preference = findPreference(key);
+    if (null != preference) {
+      // Updates the summary for the preference
+      if (!(preference instanceof CheckBoxPreference)) {
+        String value = sharedPreferences.getString(preference.getKey(), "");
+        setPreferenceSummary(preference, String.format("All your orders belong to %s", value));
+      }
+    }
+  }
+
+  @Override
+  public boolean onPreferenceChange(Preference preference, Object newValue) {
+    Toast error = Toast.makeText(getContext(), "Your name cannot be blank", Toast.LENGTH_SHORT);
+    String sizeKey = getString(R.string.settings_user_name_key);
+    if (preference.getKey().equals(sizeKey)) {
+      String newName = (String) newValue;
+
+      if (newName == null || newName.isEmpty()) {
+        error.show();
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getPreferenceScreen().getSharedPreferences()
+            .registerOnSharedPreferenceChangeListener(this);
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    getPreferenceScreen().getSharedPreferences()
+            .unregisterOnSharedPreferenceChangeListener(this);
+  }
+
+  /**
+   * Updates the summary for the preference
+   *
+   * @param preference The preference to be updated
+   * @param value The value that the preference was updated to
+   */
+  private void setPreferenceSummary(Preference preference, String value) {
+    if (preference instanceof ListPreference) {
+      // For list preferences, figure out the label of the selected value
+      ListPreference listPreference = (ListPreference) preference;
+      int prefIndex = listPreference.findIndexOfValue(value);
+      if (prefIndex >= 0) {
+        // Set the summary to that label
+        listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+      }
+    } else if (preference instanceof EditTextPreference) {
+      // For EditTextPreferences, set the summary to the value's simple string representation.
+      preference.setSummary(value);
+    }
+  }
+}
